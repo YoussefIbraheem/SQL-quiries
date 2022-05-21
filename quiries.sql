b459de5d6bd3e1dc8e64eacb4cbda3303e19eba2
YoussefIbraheem <youssefm.muhammed@gmail.com>
2022-05-21 17:13:10 +0200

    query

---
+++ quiries.sql
@@ -0,0 +1,12 @@
+
+-- 1st QUESTION
+
+SELECT productCode , COUNT(productCode) , priceEach FROM `orderdetails` GROUP BY productCode HAVING productCode LIKE 'S18%' AND priceEach > 100 ORDER BY priceEach DESC;
+
+-- 2nd QUESTION
+
+SELECT * FROM `payments`WHERE paymentDate LIKE '%-05'; 
+
+-- 3rd QUESTION
+
+SELECT * FROM `customers` WHERE country = 'usa' GROUP BY creditLimit HAVING phone LIKE '%5555%' ORDER BY creditLimit DESC LIMIT 1 OFFSET 4;
