a9290d7871b4091b0c836a371a5087cd92565f39
YoussefIbraheem <youssefm.muhammed@gmail.com>
2022-05-21 19:55:45 +0200

    newmod3

--- quiries.sql
+++ quiries.sql
@@ -11,7 +11,3 @@ SELECT * FROM `payments`WHERE paymentDate LIKE '%-05';
 
 SELECT * FROM `customers` WHERE country = 'usa' GROUP BY creditLimit HAVING phone LIKE '%5555%' ORDER BY creditLimit DESC LIMIT 1 OFFSET 4;
 
-
-lorem ipsum
-
-serum dolor
